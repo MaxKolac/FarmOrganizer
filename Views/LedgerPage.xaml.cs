d35c057bdddd1fd3320aa78106b082ffa67221e5
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-08-11 13:15:52 +0200

    Tweaked LedgerPage
    Ledger page now refreshes after adding a new record so it shows up. The implementation of this breaks MVVM because View is now aware of ViewModel, but oh well... *shrug*

--- Views/LedgerPage.xaml.cs
+++ Views/LedgerPage.xaml.cs
@@ -9,4 +9,11 @@ public partial class LedgerPage : ContentPage
 		InitializeComponent();
 		BindingContext = vm;
 	}
+
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+		base.OnNavigatedTo(args);
+		//View should NOT be "aware" of viewmodel, but this is kinda necessary
+		((LedgerPageViewModel)BindingContext).QueryLedgerEntries();
+    }
 }
