a0dd5548470080564043dd1aaf274022d6728383
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-08-26 13:04:02 +0200

    Added dynamic colors for QuickCalc
    Removed those ReadOnly bools. Added local resource dictionairies

--- ViewModels/QuickCalculatorViewModel.cs
+++ ViewModels/QuickCalculatorViewModel.cs
@@ -5,25 +5,35 @@ namespace FarmOrganizer.ViewModels
 {
     public partial class QuickCalculatorViewModel : ObservableObject
     {
-        [ObservableProperty]
-        public bool cropAmountReadOnly = false;
+        private readonly Color defaultColor = Color.FromRgb(255, 255, 255);
+        private readonly Color selectedColor = Color.FromRgb(166, 158, 45);
+
+        private const string cropAmountName = "cropAmount";
         [ObservableProperty]
         public string cropAmountValue;
         [ObservableProperty]
-        public bool sellRateReadOnly = false;
+        public Color cropAmountColor;
+
+        private const string sellRateName = "sellRate";
         [ObservableProperty]
         public string sellRateValue;
         [ObservableProperty]
-        public bool pureIncomeReadOnly = true;
+        public Color sellRateColor;
+
+        private const string pureIncomeName = "pureIncome";
         [ObservableProperty]
         public string pureIncomeValue;
+        [ObservableProperty]
+        public Color pureIncomeColor;
 
         protected readonly Queue<string> lastEditedControls = new();
 
         public QuickCalculatorViewModel()
         {
-            lastEditedControls.Enqueue("sellRate");
-            lastEditedControls.Enqueue("cropAmount");
+            lastEditedControls.Enqueue(cropAmountName);
+            lastEditedControls.Enqueue(sellRateName);
+            CropAmountColor = selectedColor;
+            SellRateColor = selectedColor;
         }
 
         [RelayCommand]
@@ -33,15 +43,15 @@ namespace FarmOrganizer.ViewModels
             double SellRate = Utils.CastToValue(SellRateValue);
             double PureIncome = Utils.CastToValue(PureIncomeValue);
 
-            if (CropAmountReadOnly)
+            if (lastEditedControls.Contains(pureIncomeName) && lastEditedControls.Contains(sellRateName))
             {
                 CropAmountValue = (PureIncome / SellRate).ToString();
             }
-            else if (SellRateReadOnly)
+            else if (lastEditedControls.Contains(pureIncomeName) && lastEditedControls.Contains(cropAmountName))
             {
                 SellRateValue = (PureIncome / CropAmount).ToString();
             }
-            else if (PureIncomeReadOnly)
+            else if (lastEditedControls.Contains(cropAmountName) && lastEditedControls.Contains(sellRateName))
             {
                 PureIncomeValue = (CropAmount * SellRate).ToString();
             }
@@ -50,12 +60,34 @@ namespace FarmOrganizer.ViewModels
         [RelayCommand]
         protected void LastTappedControlsChanged(string caller)
         {
+            //TODO: backgroudn colors refuse to change once they turn yello
+            if (lastEditedControls.Contains(caller))
+                return;
             lastEditedControls.Enqueue(caller);
-            lastEditedControls.Dequeue();
-
-            CropAmountReadOnly = !lastEditedControls.Contains("cropAmount");
-            SellRateReadOnly = !lastEditedControls.Contains("sellRate");
-            PureIncomeReadOnly = !lastEditedControls.Contains("pureIncome");
+            switch (lastEditedControls.Dequeue())
+            {
+                case cropAmountName:
+                    CropAmountColor = defaultColor;
+                    break;
+                case sellRateName:
+                    SellRateColor = defaultColor;
+                    break;
+                case pureIncomeName:
+                    PureIncomeColor = defaultColor;
+                    break;
+            }
+            switch (caller)
+            {
+                case cropAmountName:
+                    CropAmountColor = selectedColor;
+                    break;
+                case sellRateName:
+                    SellRateColor = selectedColor;
+                    break;
+                case pureIncomeName:
+                    PureIncomeColor = selectedColor;
+                    break;
+            }
         }
     }
 }
