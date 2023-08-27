3cadc946e2c0a3b5245ad9c54c60c1302f1ab64e
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-08-27 16:29:55 +0200

    Added reset database option in Settings
    Added a bit of doc to converter class.

--- ViewModels/SettingsPageViewModel.cs
+++ ViewModels/SettingsPageViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FarmOrganizer.Database;
 using System.Globalization;
 
 namespace FarmOrganizer.ViewModels
@@ -17,6 +19,20 @@ namespace FarmOrganizer.ViewModels
             ApplyPreferences();
         }
 
+        [RelayCommand]
+        private async static Task ResetDatabase()
+        {
+            if (await App.AlertSvc.ShowConfirmationAsync(
+                "Uwaga!",
+                "Za chwilę bezpowrotnie wyczyścisz wszystkie dane z bazy danych. " +
+                "Tej akcji nie można odwrócić. Czy jesteś pewny aby kontynuować?", 
+                "Tak", "Nie"))
+            {
+                DatabaseFile.Delete();
+                await MainThread.InvokeOnMainThreadAsync(DatabaseFile.Create);
+            }
+        }
+
         public static void ApplyPreferences()
         {
             Application.Current.UserAppTheme = Enum.Parse<AppTheme>(Preferences.Get("appTheme", Enum.GetName(AppTheme.Unspecified)));
@@ -25,6 +41,9 @@ namespace FarmOrganizer.ViewModels
 
     internal class StringToAppThemeConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts an AppTheme enum into a string, containing its name.
+        /// </summary>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is not AppTheme)
@@ -32,6 +51,9 @@ namespace FarmOrganizer.ViewModels
             return Enum.GetName((AppTheme)value);
         }
 
+        /// <summary>
+        /// Converts a string into an AppTheme enum with the matching name.
+        /// </summary>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is not string)
