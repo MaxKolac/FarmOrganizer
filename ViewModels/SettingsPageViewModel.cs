4e5f016c9d1c797040f5e57ed158c4f68f334c3f
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-08-27 16:06:33 +0200

    Added app theme preferences
    I love Maui.Storage.Preferences and AppTheme enums.
    Also encapsulated static fields in App class so IntelliSense finally shuts up about it.

--- ViewModels/SettingsPageViewModel.cs
+++ ViewModels/SettingsPageViewModel.cs
@@ -1,8 +1,47 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Globalization;
 
 namespace FarmOrganizer.ViewModels
 {
     public partial class SettingsPageViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private List<string> appThemes = Enum.GetNames<AppTheme>().ToList();
+        [ObservableProperty]
+        private AppTheme selectedTheme = 
+            Enum.Parse<AppTheme>(Preferences.Get("appTheme", Enum.GetName(AppTheme.Unspecified)));
+
+        partial void OnSelectedThemeChanging(AppTheme oldValue, AppTheme newValue)
+        {
+            Preferences.Set("appTheme", Enum.GetName(newValue));
+            ApplyPreferences();
+        }
+
+        public static void ApplyPreferences()
+        {
+            Application.Current.UserAppTheme = Enum.Parse<AppTheme>(Preferences.Get("appTheme", Enum.GetName(AppTheme.Unspecified)));
+        }
+    }
+
+    internal class StringToAppThemeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not AppTheme)
+                return string.Empty;
+            return Enum.GetName((AppTheme)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string)
+                return AppTheme.Unspecified;
+            foreach (string theme in Enum.GetNames<AppTheme>().ToList())
+            {
+                if (theme == (string)value)
+                    return Enum.Parse<AppTheme>(theme);
+            }
+            return AppTheme.Unspecified;
+        }
     }
 }
