5250416c806e92a0e16ac23f5ea1c864db99a05e
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-08-29 12:24:50 +0200

    Expanded the DbContext and Models
    Added Seasons and FieldEfficiency tables.
    Added isExpense property to CostTypes
    Added foreign key of Seasons to BalanceLedger

---
+++ Models/FieldEfficiency.cs
@@ -0,0 +1,13 @@
+﻿namespace FarmOrganizer.Models;
+
+public partial class FieldEfficiency
+{
+    public int Id { get; set; }
+    public int IdCropField { get; set; }
+    public int IdSeason { get; set; }
+    public double Hectares { get; set; }
+    public double CropAmount { get; set; }
+
+    public virtual CropField IdCropFieldNavigation { get; set; }
+    public virtual Season IdSeasonNavigation { get; set; }
+}
