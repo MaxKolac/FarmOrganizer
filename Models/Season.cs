b5dbf2ab4105b577ca43121cd03e7a0d6d0cad02
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-09-04 14:48:29 +0200

    Tweaked ToString() method

--- Models/Season.cs
+++ Models/Season.cs
@@ -2,6 +2,11 @@
 
 namespace FarmOrganizer.Models;
 
+/// <summary>
+/// Represents a period of time, during which costs and profits can be accumulated.
+/// <see cref="DateTime"/> of later than 9999.01.01 is treated as "in progress".
+/// Only one season in the Seasons table maybe be "opened" - otherwise things will break.
+/// </summary>
 public partial class Season
 {
     public int Id { get; set; }
@@ -17,7 +22,9 @@ public partial class Season
     {
         StringBuilder builder = new();
         builder.Append(Name + " (" + DateStart.ToString("dd.MM.yy") + " - ");
-        builder.Append(HasConcluded ? "dzisiaj) - Zakończony" : DateEnd.ToString("dd.MM.yy") + ")");
+        builder.Append(new DateTime(9999, 1, 1) < DateEnd ? "dzisiaj)" : DateEnd.ToString("dd.MM.yy") + ")");
+        builder.Append(" - ");
+        builder.Append(HasConcluded ? "Zakończony" : "W trakcie");
         return builder.ToString();
     }
 }
