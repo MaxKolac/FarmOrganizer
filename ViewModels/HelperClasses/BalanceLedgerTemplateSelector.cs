3af2149eb2b524ca2d9c3a21dae0a09d83b3aaff
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-09-17 15:03:57 +0200

    TODO: amend me in future, but DONT PUSH

---
+++ ViewModels/HelperClasses/BalanceLedgerTemplateSelector.cs
@@ -0,0 +1,15 @@
+﻿using FarmOrganizer.Models;
+
+namespace FarmOrganizer.ViewModels.HelperClasses
+{
+    public partial class BalanceLedgerTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate InvalidTemplate { get; set; }
+        public DataTemplate ValidTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return ((BalanceLedger)item).IdCostTypeNavigation.IsExpense ? ValidTemplate : InvalidTemplate;
+        }
+    }
+}
