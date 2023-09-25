2a4f1775de2b12ac69f2eb506264527807979286
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-09-25 20:37:56 +0200

    Massive changes to Season model
    HasConcluded has been marked as Obsolete property. It doesn't need to be deleted as of right now, but if in the future the DB schema will require any changes, this property should be removed.
    Seasons table no longer requires to have exactly 1 record with HasConcluded set to true.
    Complete rewrite of IValidatable interface implementations.
    Added some docs.

--- Models/Season.cs
+++ Models/Season.cs
@@ -5,16 +5,26 @@ using System.Text;
 namespace FarmOrganizer.Models;
 
 /// <summary>
-/// Represents a period of time, during which costs and profits can be accumulated.
-/// <see cref="DateTime"/> of later than 9999.01.01 is treated as "in progress".
-/// Only one season in the Seasons table may be "opened" - otherwise things will break.
+/// Represents a period of time, during which costs and profits can be accumulated. <br/>
+/// A collection of Seasons added together chronologically should perfectly recreate a timeline from 01.01.2023 to 12.31.9999 without any dates left out.
 /// </summary>
 public partial class Season : IValidatable<Season>
 {
     public int Id { get; set; }
+    /// <summary>
+    /// Purely cosmetic string property, holding a user-friendly name of the <see cref="Season"/>.
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// The <see cref="DateTime"/> representing the beginning of a <see cref="Season"/>. Its <see cref="DateTime.TimeOfDay"/> property should have a value of 00:00:00.
+    /// </summary>
     public DateTime DateStart { get; set; }
+    /// <summary>
+    /// The <see cref="DateTime"/> representing the end of a <see cref="Season"/>. Its <see cref="DateTime.TimeOfDay"/> property should have a value of 23:59:99.<br/>
+    /// Values greater than date of 01.01.9999 indicate that this <see cref="Season"/> is chronologically last added.
+    /// </summary>
     public DateTime DateEnd { get; set; }
+    [Obsolete("The program is no longer making use of this property, it will be deleted on the next database schema update.")]
     public bool HasConcluded { get; set; }
 
     public virtual ICollection<BalanceLedger> BalanceLedgers { get; set; } = new List<BalanceLedger>();
@@ -25,8 +35,8 @@ public partial class Season : IValidatable<Season>
         StringBuilder builder = new();
         builder.Append(Name + " (" + DateStart.ToString("dd.MM.yy") + " - ");
         builder.Append(new DateTime(9999, 1, 1) < DateEnd ? "dzisiaj)" : DateEnd.ToString("dd.MM.yy") + ")");
-        builder.Append(" - ");
-        builder.Append(HasConcluded ? "Zakończony" : "W trakcie");
+        //builder.Append(" - ");
+        //builder.Append(HasConcluded ? "Zakończony" : "W trakcie");
         return builder.ToString();
     }
 
@@ -43,59 +53,164 @@ public partial class Season : IValidatable<Season>
         if (allSeasons.Count == 0)
             throw new TableValidationException(nameof(DatabaseContext.Seasons), "Nie odnaleziono żadnych rekordów.");
 
-        //Check if the only season is concluded
-        if (allSeasons.Count == 1 && allSeasons[0].HasConcluded)
+        //Check if any DateEnd and DateStart doesnt follow the pattern of having TimeOfDay property set to 23:59:99 and 00:00:00 respectively, except for the last Season
+        for (int i = 0; i < allSeasons.Count - 1; i++)
         {
-            var season = allSeasons[0];
-            throw new TableValidationException(nameof(DatabaseContext.Seasons), "W tabeli znajduje się tylko jeden sezon, który jest zakończony", season.ToString(), nameof(season.HasConcluded));
+            if (allSeasons[i].DateEnd != allSeasons[i + 1].DateEnd.AddMicroseconds(-1))
+                throw new TableValidationException(nameof(DatabaseContext.Seasons), "Wykryto dwa sezony, których daty rozpoczęcia i zakończenia nie pokrywają się prawidłowo.", $"{allSeasons[i]}; {allSeasons[i + 1]}", "DateEnd; DateStart");
         }
 
-        List<Season> openSeasons = allSeasons.FindAll(e => !e.HasConcluded);
-
-        //Check if there are any open seasons
-        if (openSeasons.Count == 0)
-            throw new TableValidationException(nameof(DatabaseContext.Seasons), "W tabeli nie odnaleziono żadnych otwartych sezonów.");
-
-        //Check if there's more than 1 open season.
-        if (openSeasons.Count > 1)
-            throw new TableValidationException(nameof(DatabaseContext.Seasons), "W tabeli odnaleziono więcej niż jeden otwarty sezon.");
+        //Check if last Season has the MaxValue on DateEnd
+        if (allSeasons[^1].DateEnd != DateTime.MaxValue)
+            throw new TableValidationException(nameof(DatabaseContext.Seasons), "Końcowy sezon musi posiadać DateTime.MaxValue jako datę zakończenia.");
     }
 
+    /// <summary>
+    /// <inheritdoc/>
+    /// <para>
+    /// The only property that matters for <see cref="Season"/> is <see cref="DateStart"/> and <see cref="Name"/>.
+    /// <see cref="DateStart"/> will be automatically formatted so that:
+    /// <list type="bullet">
+    /// <item>The previous <see cref="Season"/> ends on the <paramref name="entry"/>'s DateStart with time being 23:59:99.</item>
+    /// <item>The added <see cref="Season"/> starts on the day after <paramref name="entry"/>'s DateStart with time being 00:00:00.</item>
+    /// </list>
+    /// </para>
+    /// </summary>
+    /// <inheritdoc/>
     public static void AddEntry(Season entry)
     {
         using var context = new DatabaseContext();
-        Season seasonToEnd = context.Seasons.Find(GetCurrentSeason().Id);
+        Season lastSeason = context.Seasons.OrderBy(season => season.DateStart).Last();
 
         //New season cannot have empty name
         if (string.IsNullOrEmpty(entry.Name))
             throw new InvalidRecordPropertyException("Nazwa", null, "Sezon musi posiadać niepustą nazwę.");
 
         //New season cannot start before the previous one started
-        if (seasonToEnd.DateStart >= entry.DateStart)
+        if (lastSeason.DateStart >= entry.DateStart.Date.AddDays(1).AddMicroseconds(-1))
             throw new InvalidRecordPropertyException("Data rozpoczęcia", entry.DateStart.ToShortDateString(), "Data rozpoczęcia nowego sezonu nie może być wcześniej od daty zakończenia obecnego sezonu.");
 
-        seasonToEnd.HasConcluded = true;
-        seasonToEnd.DateEnd = entry.DateStart;
-        context.Seasons.Add(entry);
+        lastSeason.DateEnd = entry.DateStart.Date.AddDays(1).AddMicroseconds(-1);
+        Season entryValidated = new()
+        {
+            Name = entry.Name,
+            DateStart = entry.DateStart.Date.AddDays(1),
+            DateEnd = DateTime.MaxValue
+        };
+        context.Seasons.Add(entryValidated);
         context.SaveChanges();
     }
 
+    /// <summary>
+    /// <inheritdoc/>
+    /// <para>
+    /// <list type="bullet">
+    /// <item>If a previous <see cref="Season"/> exists, its <see cref="DateEnd"/> is set to day before <paramref name="entry"/>.DateStart.Date with a time of 23:59:99 and the edited <see cref="DateStart"/> is set to <paramref name="entry"/>.DateStart.Date with time of 00:00:00.</item>
+    /// <item>If a next <see cref="Season"/> exists, its <see cref="DateStart"/> is set to the day after <paramref name="entry"/>.DateEnd with a time of 00:00:00 and the edited <see cref="DateEnd"/> is set to <paramref name="entry"/>.DateEnd.Date with a time of 23:59:99.</item>
+    /// </list>
+    /// </para>
+    /// </summary>
+    /// <inheritdoc/>
     public static void EditEntry(Season entry)
     {
-        throw new NotImplementedException();
-    }
+        #nullable enable
+        using var context = new DatabaseContext();
+        Season editedSeason = context.Seasons.Find(entry.Id) ?? throw new NoRecordFoundException(nameof(DatabaseContext.Seasons), $"Id == {entry.Id}");
+        Season? previousSeason = null;
+        Season? nextSeason = null;
 
-    public static void DeleteEntry(Season entry)
-    {
-        throw new NotImplementedException();
+        //New season cannot have empty name
+        if (string.IsNullOrEmpty(entry.Name))
+            throw new InvalidRecordPropertyException("Nazwa", null, "Sezon musi posiadać niepustą nazwę.");
+
+        //Find out if the edited Season has any chronological neighbours.
+        //If it does, their respective DateTime property will also have to be changed, as to not leave out any gaps in timeline
+        List<Season> allSeasons = context.Seasons.OrderBy(season => season.DateStart).ToList();
+        for (int i = allSeasons.Count - 1; i >= 0; i--)
+        {
+            if (allSeasons[i].Id == entry.Id)
+            {
+                previousSeason = context.Seasons.Find(i - 1);
+                nextSeason = context.Seasons.Find(i + 1);
+                break;
+            }
+        }
+
+        //Date restraints depending on whether or not Season being edited has neighbouring Seasons
+        if (previousSeason is not null)
+        {
+            if (entry.DateStart.Date <= previousSeason.DateStart.Date.AddMicroseconds(-1))
+                throw new InvalidRecordPropertyException("Data rozpoczęcia", entry.DateStart.Date.ToString(), "Przesuń datę rozpoczęcia na późniejszą datę.");
+            previousSeason.DateEnd = entry.DateStart.Date.AddMicroseconds(-1);
+            editedSeason.DateStart = entry.DateStart.Date;
+        }
+
+        if (nextSeason is not null)
+        {
+            if (nextSeason.DateEnd <= entry.DateEnd.Date.AddDays(1).AddMicroseconds(-1))
+                throw new InvalidRecordPropertyException("Data zakończenia", entry.DateEnd.Date.ToString(), "Przesuń datę zakończenia na wcześniejszą datę.");
+            editedSeason.DateEnd = entry.DateEnd.Date.AddDays(1).AddMicroseconds(-1);
+            nextSeason.DateStart = entry.DateEnd.Date.AddDays(1);
+        }
+        context.SaveChanges();
     }
 
     /// <summary>
-    /// Retrieves the record of the currently open season. This method does NOT check consistency before retrieving the record.
+    /// <inheritdoc/>
+    /// <para>
+    /// If the deleted <see cref="Season"/> is chronologically the last, the previous record's <see cref="DateEnd"/> is changed to <see cref="DateTime.MaxValue"/>.<br/>
+    /// Otherwise, the <see cref="Season"/> right after the deleted one fills in the gap left behind after deleting the desired record.
+    /// </para>
+    /// </summary>
+    /// <inheritdoc/>
+    public static void DeleteEntry(Season entry)
+    {
+        using var context = new DatabaseContext();
+        Season? seasonToDelete = context.Seasons.Find(entry.Id);
+        if (seasonToDelete is null)
+            return;
+        
+        List<Season> allSeasons = context.Seasons.OrderBy(season => season.DateStart).ToList();
+
+        //Minimum of 1 season
+        if (allSeasons.Count <= 1)
+            throw new RecordDeletionException("Sezony");
+
+        //Find out if the deleted Season is the last Season.
+        if (allSeasons[^1].Id == seasonToDelete.Id)
+        {
+            Season? previousSeason = context.Seasons.Find(allSeasons[^2].Id) ?? 
+                throw new RecordDeletionException("Sezony", "Odnaleziono więcej niż 1 sezon, ale nie udało się odnaleźć drugiego sezonu licząc od końca.");
+            previousSeason.DateEnd = DateTime.MaxValue;
+            return;
+        }
+        //If it isn't, the next season is chosen to cover the timespan hole left after the entry is deleted
+        else
+        {
+            for (int i = allSeasons.Count - 2; i >= 0; i--)
+            {
+                if (allSeasons[i].Id == entry.Id)
+                {
+                    Season? nextSeason = context.Seasons.Find(i + 1) ??
+                        throw new RecordDeletionException("Sezony", "Nie odnaleziono sezonu następującego po sezonie usuwanym, mimo że powinien istnieć.");
+                    nextSeason.DateStart = seasonToDelete.DateStart;
+                }
+            }
+        }
+        context.Seasons.Remove(seasonToDelete);
+        context.SaveChanges();
+    }
+
+    /// <summary>
+    /// Retrieves the record of the Season currently in progress, timewise. This method does NOT check consistency before retrieving the record.
     /// </summary>
     public static Season GetCurrentSeason()
     {
         using var context = new DatabaseContext();
-        return context.Seasons.Where(season => !season.HasConcluded).First();
+        IEnumerable<Season> query =
+            from season in context.Seasons
+            where season.DateStart <= DateTime.Now && DateTime.Now <= season.DateEnd
+            select season;
+        return query.First();
     }
 }
