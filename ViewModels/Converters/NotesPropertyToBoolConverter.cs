dc58b5d4b2e7b6fa6f033dc67ea7b7dd3acd03f2
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-10-01 15:58:50 +0200

    DataTemplate for BalanceLedger no longer displays Label if Notes are empty

---
+++ ViewModels/Converters/NotesPropertyToBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿using FarmOrganizer.Models;
+using FarmOrganizer.Views;
+using System.Globalization;
+
+namespace FarmOrganizer.ViewModels.Converters
+{
+    /// <summary>
+    /// Used in <see cref="LedgerPage"/> to hide DataTemplate's Label when <see cref="BalanceLedger.Notes"/> is empty. 
+    /// </summary>
+    public class NotesPropertyToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string)
+                return false;
+            string note = (string)value;
+            return !string.IsNullOrEmpty(note);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
