d593fc0e377b0e82b966671916bb1bd2d38f56e5
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-10-01 18:11:43 +0200

    Fixed a name duplicate app crash on exporting
    Caused when trying to export a DB file to a folder which already had a file named "database.sqlite3". Now export method can create a file name with a number at the end to always have a unique name for it.

--- Database/DatabaseFile.cs
+++ Database/DatabaseFile.cs
@@ -7,40 +7,34 @@ namespace FarmOrganizer.Database
     /// </summary>
     public static class DatabaseFile
     {
+        public static string Filename => "database";
+        public static string Extension => ".sqlite3";
         /// <summary>
         /// The name of the database's file. It needs to be the same as the DB file in the <c>Resources</c> folder.
         /// </summary>
-        public static string Filename => "database.sqlite3";
+        public static string FullFilename => Filename + Extension;
         /// <summary>
         /// The name of a temporary copy of the database's file.
         /// </summary>
-        public static string BackupFilename => Filename + ".temp";
+        public static string BackupFilename => FullFilename + ".temp";
         /// <summary>
         /// The path of the folder, in which the database file is stored.
         /// </summary>
-        public static string Location => FileSystem.Current.AppDataDirectory;
+        public static string InternalStorage => FileSystem.Current.AppDataDirectory;
         /// <summary>
         /// The full qualified path to the database's file. Combination of its location and name.
         /// </summary>
-        public static string FullPath => Path.Combine(FileSystem.Current.AppDataDirectory, Filename);
+        public static string FullPath => Path.Combine(FileSystem.Current.AppDataDirectory, FullFilename);
 
         /// <summary>
         /// Copy the read-only fresh database file bundled with the APK to app's external storage to be editable.
-        /// You can dispatch the creation of a new file with the following sample:
-        /// <code>
-        /// Application.Current.MainPage.Dispatcher.Dispatch(async () => await DatabaseFile.Create());
-        /// </code>
-        /// Alternatively, you can invoke it on the main thread using the following sample:
-        /// <code>
-        /// MainThread.InvokeOnMainThreadAsync(DatabaseFile.Create);
-        /// </code>
         /// <para>
         /// <see href="https://learn.microsoft.com/en-us/dotnet/maui/platform-integration/storage/file-system-helpers?tabs=android"/>
         /// </para>
         /// </summary>
         public static async Task Create()
         {
-            using Stream inputStream = await FileSystem.Current.OpenAppPackageFileAsync(Filename);
+            using Stream inputStream = await FileSystem.Current.OpenAppPackageFileAsync(FullFilename);
             using FileStream outputStream = File.Create(FullPath);
             await inputStream.CopyToAsync(outputStream);
         }
@@ -69,9 +63,15 @@ namespace FarmOrganizer.Database
         /// <param name="destinationFolderPath">The destination folder to copy the file to.</param>
         public static async Task ExportTo(string destinationFolderPath)
         {
-            using Stream dbFile = File.OpenRead(FullPath);
-            using FileStream outputStream = File.Create(Path.Combine(destinationFolderPath, Filename));
-            await dbFile.CopyToAsync(outputStream);
+            string actualFilename = FullFilename;
+            int attempts = 0;
+            while (File.Exists(Path.Combine(destinationFolderPath, actualFilename)))
+            {
+                attempts++;
+                actualFilename = $"{Filename}({attempts})" + Extension;
+            };
+            using FileStream outputStream = File.Create(Path.Combine(destinationFolderPath, actualFilename));
+            await File.OpenRead(FullPath).CopyToAsync(outputStream);
         }
 
         /// <summary>
@@ -105,7 +105,7 @@ namespace FarmOrganizer.Database
         /// </summary>
         public static async Task CreateBackup()
         {
-            using FileStream dbBackupFile = File.Create(Path.Combine(Location, BackupFilename));
+            using FileStream dbBackupFile = File.Create(Path.Combine(InternalStorage, BackupFilename));
             using FileStream database = File.OpenRead(FullPath);
             await database.CopyToAsync(dbBackupFile);
         }
@@ -116,7 +116,7 @@ namespace FarmOrganizer.Database
         /// <param name="deleteOnCompletion">Should the used backup file be deleted after restoring.</param>
         public static async Task RestoreBackup(bool deleteOnCompletion = true)
         {
-            using (FileStream dbBackupFile = File.OpenRead(Path.Combine(Location, BackupFilename)))
+            using (FileStream dbBackupFile = File.OpenRead(Path.Combine(InternalStorage, BackupFilename)))
             {
                 using FileStream database = File.Create(FullPath);
                 await dbBackupFile.CopyToAsync(database);
@@ -129,7 +129,7 @@ namespace FarmOrganizer.Database
         /// Deletes the backup file of the database.
         /// </summary>
         public static void DeleteBackup() =>
-            File.Delete(Path.Combine(Location, BackupFilename));
+            File.Delete(Path.Combine(InternalStorage, BackupFilename));
 
         /// <summary>
         /// <para>
