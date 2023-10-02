44835aa395b80c65137d7977dd92cc6e60cff480
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-10-02 12:34:12 +0200

    Workaround for broken DataTemplateSelector
    Just make it a converter that converts entry's CostType to a proper color of BalanceChange's label text.

---
+++ ViewModels/Converters/CostTypeToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using FarmOrganizer.Models;
+using System.Globalization;
+
+namespace FarmOrganizer.ViewModels.Converters
+{
+    /// <summary>
+    /// A workaround for bugged DataTemplate Selector.<br/>
+    /// Used in <see cref="Views.LedgerPage"/> to change the balance change's text color to be red when it's a cost
+    /// </summary>
+    public class CostTypeToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not CostType)
+                return Colors.Black;
+            CostType cost = value as CostType;
+            return cost.IsExpense ? Colors.DarkRed : Colors.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
