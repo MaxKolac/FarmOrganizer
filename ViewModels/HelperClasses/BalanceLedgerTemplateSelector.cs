79d397771a78682281be5b80052e6d4efe9019c1
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-10-02 12:38:04 +0200

    Marked DataTemplateSelector as broken

--- ViewModels/HelperClasses/BalanceLedgerTemplateSelector.cs
+++ ViewModels/HelperClasses/BalanceLedgerTemplateSelector.cs
@@ -2,6 +2,11 @@
 
 namespace FarmOrganizer.ViewModels.HelperClasses
 {
+    /// <summary>
+    /// DataTemplateSelectors do not work as of current .NET MAUI version.<br/>
+    /// See commit 44835aa395b80c65137d7977dd92cc6e60cff480 or <see cref="Converters.CostTypeToColorConverter"/> for a workaround.
+    /// </summary>
+    [Obsolete("Class does not work, see the XamlDoc for a workaround.")]
     public partial class BalanceLedgerTemplateSelector : DataTemplateSelector
     {
         public DataTemplate InvalidTemplate { get; set; }
