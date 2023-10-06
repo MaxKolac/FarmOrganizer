e149365d83a608d997af3c1fbb4ea6d0e8bc8c34
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-10-06 20:37:47 +0200

    Added more tests

---
+++ test/Models/BalanceLedgerTests.cs
@@ -0,0 +1,34 @@
+﻿using FarmOrganizer.Database;
+using FarmOrganizer.Exceptions;
+using FarmOrganizer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FarmOrganizerTests.Models
+{
+    public class BalanceLedgerTests
+    {
+        public static class BalanceLedgerTestData
+        {
+            public static IEnumerable<object[]> GetInvalidRecordsToValidate()
+            {
+                yield return new object[]
+                {
+                    new BalanceLedger
+                    {
+                        BalanceChange = -0.01m
+                    }
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(BalanceLedgerTestData.GetInvalidRecordsToValidate), MemberType = typeof(BalanceLedgerTestData))]
+        public static void ValidationThrowsTest(List<BalanceLedger> recordsToMock)
+        {
+            var context = new Mock<DatabaseContext>();
+            context.Setup<DbSet<BalanceLedger>>(e => e.BalanceLedgers).ReturnsDbSet(recordsToMock);
+
+            Assert.Throws<TableValidationException>(() => CropField.Validate(context.Object));
+        }
+    }
+}
---
+++ test/Models/CropFieldTests.cs
@@ -0,0 +1,132 @@
+﻿using FarmOrganizer.Database;
+using FarmOrganizer.Exceptions;
+using FarmOrganizer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FarmOrganizerTests.Models
+{
+    public class CropFieldTests
+    {
+        public static class CropFieldTestData
+        {
+            public static Mock<DatabaseContext> GetMockWithValidData()
+            {
+                var context = new Mock<DatabaseContext>();
+                IList<CropField> records = new List<CropField>()
+                {
+                    new CropField(){ Id = 1, Name = "A", Hectares = 1.55m },
+                    new CropField(){ Id = 2, Name = "B", Hectares = 0.45m }
+                };
+                context.Setup<DbSet<CropField>>(e => e.CropFields).ReturnsDbSet(records);
+                return context;
+            }
+
+            public static IEnumerable<object[]> GetInvalidRecordsToValidate()
+            {
+                yield return new object[]
+                {
+                    new List<CropField>()
+                };
+                yield return new object[]
+                {
+                    new List<CropField>()
+                    {
+                        new CropField(){ Id = 1, Name = "A", Hectares = 1.55m },
+                        new CropField(){ Id = 2, Name = "B", Hectares = 0m }
+                    }
+                };
+                yield return new object[]
+                {
+                    new List<CropField>()
+                    {
+                        new CropField(){ Id = 1, Name = "A", Hectares = 1.55m },
+                        new CropField(){ Id = 2, Name = string.Empty, Hectares = 0.45m }
+                    }
+                };
+                yield return new object[]
+                {
+                    new List<CropField>()
+                    {
+                        new CropField(){ Id = 1, Name = "A", Hectares = 1.55m },
+                        new CropField(){ Id = 2, Name = "B", Hectares = -0.01m }
+                    }
+                };
+            }
+
+            public static IEnumerable<object[]> GetInvalidRecordsToAdd()
+            {
+                yield return new object[]
+                {
+                    new CropField(){ Name = "B", Hectares = 0m }
+                };
+                yield return new object[]
+                {
+                    new CropField(){ Name = string.Empty, Hectares = 0.45m }
+                };
+                yield return new object[]
+                {
+                    new CropField(){ Name = "B", Hectares = -0.01m }
+                };
+            }
+
+            public static IEnumerable<object[]> GetInvalidRecordsToEdit()
+            {
+                yield return new object[]
+                {
+                    new CropField(){ Id = 1, Name = "B", Hectares = 0m }
+                };
+                yield return new object[]
+                {
+                    new CropField(){ Id = 1, Name = string.Empty, Hectares = 0.45m }
+                };
+                yield return new object[]
+                {
+                    new CropField(){ Id = 1, Name = "B", Hectares = -0.01m }
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(CropFieldTestData.GetInvalidRecordsToValidate), MemberType = typeof(CropFieldTestData))]
+        public static void ValidationThrowsTest(List<CropField> recordsToMock)
+        {
+            var context = new Mock<DatabaseContext>();
+            context.Setup<DbSet<CropField>>(e => e.CropFields).ReturnsDbSet(recordsToMock);
+
+            Assert.Throws<TableValidationException>(() => CropField.Validate(context.Object));
+        }
+
+        [Theory]
+        [MemberData(nameof(CropFieldTestData.GetInvalidRecordsToAdd), MemberType = typeof(CropFieldTestData))]
+        public static void AddingInvalidRecords(CropField invalidRecord)
+        {
+            var context = CropFieldTestData.GetMockWithValidData();
+
+            Assert.Throws<InvalidRecordPropertyException>(() => CropField.AddEntry(invalidRecord, context.Object));
+        }
+
+        [Fact]
+        public static void EditNonexistentRecord()
+        {
+            var context = CropFieldTestData.GetMockWithValidData();
+            var nonexistentRecord = new CropField() { Id = 69 };
+            Assert.Throws<NoRecordFoundException>(() => CropField.EditEntry(nonexistentRecord, context.Object));
+        }
+
+        [Theory]
+        [MemberData(nameof(CropFieldTestData.GetInvalidRecordsToEdit), MemberType = typeof(CropFieldTestData))]
+        public static void EditRecordsToBeInvalid(CropField updateEntry)
+        {
+            var context = CropFieldTestData.GetMockWithValidData();
+            Assert.Throws<InvalidRecordPropertyException>(() => CropField.EditEntry(updateEntry, context.Object));
+        }
+
+        [Fact]
+        public static void DeleteLastRecord()
+        {
+            var context = new Mock<DatabaseContext>();
+            context.Setup<DbSet<CropField>>(e => e.CropFields).ReturnsDbSet(new List<CropField>() { new() { Id = 1 } });
+            Assert.Throws<RecordDeletionException>(() => CropField.DeleteEntry(1, context.Object));
+        }
+    }
+}
