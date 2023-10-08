21f78e3b7eb9c521173eee48893e30c3b6ad6bc0
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-10-08 21:47:29 +0200

    Create GenericFontResolver.cs
    Code authored by Luis Beltran: https://github.com/icebeam7

---
+++ src/Exporting/PDF/GenericFontResolver.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Reflection;
+using FarmOrganizer.Exporting.PDF;
+using PdfSharpCore.Fonts;
+
+namespace PDFDemo.Helpers
+{
+    /// <summary>
+    /// Author: <see href="https://github.com/icebeam7">Luis Beltran</see> | <see href="https://github.com/icebeam7/PDFDemo">Source GitHub repository - PDFDemo</see>
+    /// </summary>
+    public class GenericFontResolver : IFontResolver
+    {
+        public string DefaultFontName => "OpenSans";
+
+        public byte[] GetFont(string faceName)
+        {
+            if (faceName.Contains(DefaultFontName))
+            {
+                //var assembly = typeof(ProductsReport).GetTypeInfo().Assembly;
+                var assembly = typeof(PdfBuilder).GetTypeInfo().Assembly;
+                var stream = assembly.GetManifestResourceStream($"PDFDemo.Fonts.{faceName}.ttf");
+
+                using (var reader = new StreamReader(stream))
+                {
+                    var bytes = default(byte[]);
+
+                    using (var ms = new MemoryStream())
+                    {
+                        reader.BaseStream.CopyTo(ms);
+                        bytes = ms.ToArray();
+                    }
+
+                    return bytes;
+                }
+            }
+            else
+                return GetFont(DefaultFontName);
+        }
+
+        public FontResolverInfo ResolveTypeface(string familyName, bool isBold, bool isItalic)
+        {
+            //var fontName = string.Empty;
+
+            switch (familyName)
+            {
+                case "Open Sans":
+                case "OpenSans":
+                    var fontName = "OpenSans";
+
+                    if (isBold && isItalic)
+                        fontName = $"{fontName}-BoldItalic";
+                    else if (isBold)
+                        fontName = $"{fontName}-Bold";
+                    else if (isItalic)
+                        fontName = $"{fontName}-Italic";
+                    else
+                        fontName = $"{fontName}-Regular";
+
+                    return new FontResolverInfo(fontName);
+                default:
+                    break;
+            }
+
+            return null;
+        }
+    }
+}
