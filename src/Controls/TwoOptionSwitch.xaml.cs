52fda7ef89277384bbb1b5ba9b0bfc6ac77b0348
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-10-29 21:13:01 +0100

    Fixed a case where the Switch wouldn't update visually on SelectedOptionProperty change

--- src/Controls/TwoOptionSwitch.xaml.cs
+++ src/Controls/TwoOptionSwitch.xaml.cs
@@ -23,10 +23,11 @@ public partial class TwoOptionSwitch : ContentView
 
     public static readonly BindableProperty LeftOptionTextProperty = BindableProperty.Create(nameof(LeftOptionText), typeof(string), typeof(TwoOptionSwitch), "Opcja A");
     public static readonly BindableProperty RightOptionTextProperty = BindableProperty.Create(nameof(RightOptionText), typeof(string), typeof(TwoOptionSwitch), "Opcja B");
-    public static readonly BindableProperty OptionSelectedProperty = BindableProperty.Create(nameof(OptionSelected), typeof(bool), typeof(TwoOptionSwitch), defaultBindingMode: BindingMode.TwoWay, propertyChanged: (bindable, oldValue, newValue) =>
+    public static readonly BindableProperty OptionSelectedProperty = BindableProperty.Create(nameof(OptionSelected), typeof(bool), typeof(TwoOptionSwitch), false, BindingMode.TwoWay, propertyChanged: (bindable, oldValue, newValue) =>
     {
         var control = bindable as TwoOptionSwitch;
         control.optionLabel.Text = (bool)newValue ? control.RightOptionText : control.LeftOptionText;
+        control.optionSwitch.IsToggled = (bool)newValue;
     });
 
 	public TwoOptionSwitch()
