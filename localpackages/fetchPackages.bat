b3ae90132d376b1a81f9cda5b71e36e6885431d8
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-10-31 10:56:16 +0100

    Replaced Nuget packages with raw DLLs
    Uses the latest release of the forked PdfSharpCore

--- localpackages/fetchPackages.bat
+++ localpackages/fetchPackages.bat
@@ -6,9 +6,9 @@ SET DestinationDir="C:\Users\ja\Documents\GitHub\FarmOrganizer\localpackages"
 
 REM PC - TODO
 
-COPY /Y %PdfSharpCoreRootDir%\MigraDocCore.DocumentObjectModel\bin\Release\*.nupkg %DestinationDir%
-COPY /Y %PdfSharpCoreRootDir%\MigraDocCore.Rendering\bin\Release\*.nupkg %DestinationDir%
-COPY /Y %PdfSharpCoreRootDir%\PdfSharpCore\bin\Release\*.nupkg %DestinationDir%
-COPY /Y %PdfSharpCoreRootDir%\PdfSharpCore.Charting\bin\Release\*.nupkg %DestinationDir%
+COPY /Y %PdfSharpCoreRootDir%\MigraDocCore.DocumentObjectModel\bin\Release\net7.0\MigraDocCore.DocumentObjectModel.dll %DestinationDir%
+COPY /Y %PdfSharpCoreRootDir%\MigraDocCore.Rendering\bin\Release\net7.0\MigraDocCore.Rendering.dll %DestinationDir%
+COPY /Y %PdfSharpCoreRootDir%\PdfSharpCore\bin\Release\net7.0\\PdfSharpCore.dll %DestinationDir%
+COPY /Y %PdfSharpCoreRootDir%\PdfSharpCore.Charting\bin\Release\net7.0\\PdfSharpCore.Charting.dll %DestinationDir%
 
 PAUSE
