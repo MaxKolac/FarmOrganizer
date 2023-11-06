672bca1f01d752889042433e1c0815a25556331a
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-11-06 14:24:21 +0100

    Implemented logic and basic view

--- src/ViewModels/PopupPageViewModel.cs
+++ src/ViewModels/PopupPageViewModel.cs
@@ -1,9 +1,28 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace FarmOrganizer.ViewModels
 {
     public partial class PopupPageViewModel : ObservableObject
     {
+        [ObservableProperty]
+        string title;
+        [ObservableProperty]
+        string description;
+        [ObservableProperty]
+        bool isConfirmable;
 
+        /// <summary>
+        /// Allows to set the popup's information shown to the user through a single method.
+        /// </summary>
+        /// <param name="title">The bolder label shown at the top of the popup.</param>
+        /// <param name="description">The label describing a confirmable action or information to the user.</param>
+        /// <param name="isConfirmable">If <c>true</c> the user will be presented with a choice between "Yes" and "No" buttons. Otherwise, only a single "OK" dismissing button is shown.</param>
+        public void SetInfo(string title, string description, bool isConfirmable = false)
+        {
+            Title = title;
+            Description = description;
+            IsConfirmable = isConfirmable;
+        }
     }
-}
+}        
--- src/Views/PopupPage.xaml.cs
+++ src/Views/PopupPage.xaml.cs
@@ -9,5 +9,13 @@ public partial class PopupPage : Popup
 	{
 		InitializeComponent();
         BindingContext = vm;
+        Closed += (sender, e) =>
+        {
+            if (e.WasDismissedByTappingOutsideOfPopup)
+                ((PopupPage)sender).OnDeclined(sender, null);
+        };
 	}
+
+    async void OnDeclined(object sender, EventArgs e) => await CloseAsync(false);
+    async void OnAccepted(object sender, EventArgs e) => await CloseAsync(true);
 }
